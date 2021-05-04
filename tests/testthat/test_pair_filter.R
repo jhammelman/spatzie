3423951d1e67bf57bc79a323a774c4c457f63b2f
Konstantin Krismer <krismer@mit.edu>
2021-05-04 09:32:25 -0400

    Fix test data reference

--- tests/testthat/test_pair_filter.R
+++ tests/testthat/test_pair_filter.R
@@ -1,7 +1,7 @@
 context("filter_pair_motifs")
-data("anchor_pair_example_countcorr", package = "spatzie")
+data("anchor_pair_example_count", package = "spatzie")
 test_that("motif pairs filtered by multiple hypothesis corrected significance", {
-  filter_pairs <- filter_pair_motifs(anchor_pair_example_countcorr,
+  filter_pairs <- filter_pair_motifs(anchor_pair_example_count,
                                      threshold = 0.5)
   data("filter_pairs_example", package = "spatzie")
   expect_equal(filter_pairs_example$pair_motif_enrich_sig,
